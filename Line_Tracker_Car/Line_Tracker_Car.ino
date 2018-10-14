a72fb7a5d62a481f084b25afbd7fba2d2e87d19f
Kiet Lam <33382487+LKietam@users.noreply.github.com>
2018-10-14 12:24:32 -0700

    Add files via upload

---
+++ Line_Tracker_Car/Line_Tracker_Car.ino
@@ -0,0 +1,93 @@
+  int stopc=0;
+  int IN1=11;
+  int IN2=9;
+  int IN3=8;
+  int IN4=7;
+  int ENA=6;
+  int ENB=5;
+  int ABS =140;
+  void mForward(){
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+}
+void mBack(){
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+void mLeft(){
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+  digitalWrite(IN1,HIGH);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
+}
+void mRight(){
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,HIGH);
+}
+void mStop(){
+  digitalWrite(ENA,LOW);
+  digitalWrite(ENB,LOW);
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENB,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int num1,num2,num3;
+  num1=digitalRead(2);
+  num2=digitalRead(4);
+  num3=digitalRead(10);
+  if((num1==1)&&num2&&num3){
+    stopc++;
+  }
+ if(stopc==0){
+  if((num1==0)&&num2&&num3){
+    mLeft();
+    delay(2);
+    while(1){
+      num2=digitalRead(4);
+      if(num2==1){
+        mLeft();
+        delay(2);
+      }else
+      break;
+    }
+   }else if(num2&&num1&&(num3==0)){
+    mRight();
+    delay(2);
+    while(1){
+      num2=digitalRead(4);
+      if(num2==1){
+        mRight();
+        delay(2);
+      }else
+      break;
+    }
+   }
+   else{
+    mForward();
+    delay(2);
+   }
+ }
+}
---
+++ Thermostat/Thermostat.ino
@@ -0,0 +1,84 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#define TEMP_ADDR 72
+LiquidCrystal_I2C lcd(0x27,16,2);
+const int Led=11;
+byte degree[8]={B00110, B01001, B01001, B00110, B00000, B00000, B00000, B00000};
+byte LED_On[8]={B00100,B10101,B01110,B11111,B01110,B10101,B00100,B00000};
+byte LED_Off[8]={B00100, B00100,B00100,B11111,B00100,B00100,B00100,B00000};
+boolean one_time=false;
+int set_temp=23;
+const int DOWN_BUTTON=8;
+const int UP_BUTTON=9;
+boolean lastDownTempButton= LOW;
+boolean currentDownTempButton= LOW;
+boolean lastUpTempButton= LOW;
+boolean currentUpTempButton= LOW;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Led,OUTPUT);
+  lcd.begin();
+  Wire.begin();
+  lcd.createChar(0,degree);
+  lcd.createChar(2,LED_On);
+  lcd.createChar(1,LED_Off);
+  lcd.setCursor(0,0);
+  lcd.print("Current:");
+  lcd.setCursor(10,0);
+  lcd.write((byte)0);
+  lcd.setCursor(11,0);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Set: ");
+  lcd.setCursor(10,1);
+  lcd.write((byte)0);
+  lcd.setCursor(11,1);
+  lcd.print("C");
+  lcd.setCursor(15,1);
+  lcd.write(1);
+}
+
+boolean debounce(boolean last, int pin){
+  boolean current=digitalRead(pin);
+  if(last!=current){
+    delay(5);
+    current= digitalRead(pin);
+  }
+  return current;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(TEMP_ADDR);
+  Wire.write(0);
+  Wire.endTransmission(TEMP_ADDR);
+  Wire.requestFrom(TEMP_ADDR,1);
+
+  while(Wire.available()==0);
+  int c=Wire.read();
+  lcd.setCursor(8,0);
+  lcd.print(c);
+
+  currentDownTempButton= debounce(lastDownTempButton, DOWN_BUTTON);
+  currentUpTempButton= debounce(lastUpTempButton, UP_BUTTON);
+
+  if(lastDownTempButton== LOW && currentDownTempButton == HIGH){
+    set_temp--; 
+  }else if(lastUpTempButton== LOW && currentUpTempButton == HIGH){
+    set_temp++;
+  }
+  lcd.setCursor(8,1);
+  lcd.print(set_temp);
+  lastDownTempButton= currentDownTempButton;
+  lastUpTempButton= currentUpTempButton;
+  if(c>=set_temp){
+    digitalWrite(Led,LOW);
+    lcd.setCursor(15,1);
+    lcd.write(2);
+  }
+  else{
+    one_time=false;
+    digitalWrite(Led,HIGH);
+    lcd.setCursor(15,1);
+    lcd.write(1);
+  }
+}
